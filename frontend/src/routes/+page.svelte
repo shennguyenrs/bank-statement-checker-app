6dcb6d2e8b83fff166f7365433448576717c2269
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2024-09-16 21:57:14 +0300

    feat(fe): simple styling

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -47,28 +47,41 @@
 	This application is used to search for records that have been imported from bank statement PDF
 	files
 </p>
-<div class="h-20" />
+<div class="h-10" />
 <div class="flex justify-between">
 	<input
 		type="text"
 		name="description"
 		placeholder="Search keyword in description..."
-		class="w-1/3"
+		class="w-1/3 rounded-md border border-gray-50 bg-gray-50 px-2"
 		bind:value={description}
 	/>
 	<div class="flex gap-2">
-		<select name="sort" placeholder="Sort by" bind:value={sortBy}>
+		<select
+			class="rounded-md border border-gray-50 bg-gray-50 px-2"
+			name="sort"
+			placeholder="Sort by"
+			bind:value={sortBy}
+		>
 			<option value="date.asc">Date from earliest</option>
 			<option value="date.esc">Date from latest</option>
 			<option value="credit.asc">Amount largest to smallest</option>
 			<option value="credit.esc">Amount smallest to largest</option>
 		</select>
-		<select name="size" placeholder="Page size" bind:value={pageSize}>
+		<select
+			class="rounded-md border border-gray-50 bg-gray-50 px-2"
+			name="size"
+			placeholder="Page size"
+			bind:value={pageSize}
+		>
 			<option value={10}>10</option>
 			<option value={20}>20</option>
 			<option value={50}>50</option>
 		</select>
-		<button on:click={handleFilter}>Filter</button>
+		<button
+			class="rounded-md bg-zinc-800 px-4 py-2 text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-zinc-700 active:scale-95"
+			on:click={handleFilter}>Filter</button
+		>
 	</div>
 </div>
 <div class="h-8" />
