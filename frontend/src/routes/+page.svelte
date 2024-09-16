d20cbae12f90fc711b6fe7cb8009b8740a0b4cc8
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2024-09-16 16:29:02 +0300

    feat: frontend connect to backend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,44 @@
 <script lang="ts">
+	import qs from 'qs';
+
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	let rows = data.data.rows;
+	$: rows = data.data.rows;
+	$: description = '';
+	$: sortBy = 'date.asc';
+	$: pageSize = 10;
+	$: currentPage = 1;
 
 	function formatMontery(amount: number) {
 		return amount.toLocaleString('vi-VN');
+	}
+
+	async function handleFilter() {
+		const sortParts = sortBy.split('.');
+		const query = qs.stringify(
+			{
+				page: currentPage,
+				pageSize,
+				description,
+				sortBy: sortParts[0],
+				sort: sortParts[1]
+			},
+			{
+				encodeValuesOnly: true
+			}
+		);
+
+		const url = `${import.meta.env.VITE_API_BASE_URL}/statements?${query}`;
+		const res = await fetch(url, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`
+			}
+		});
+		const data = await res.json();
+		rows = data.rows;
 	}
 </script>
 
@@ -16,20 +49,26 @@
 </p>
 <div class="h-20" />
 <div class="flex justify-between">
-	<input type="text" name="search" placeholder="Search keyword in description..." class="w-1/3" />
+	<input
+		type="text"
+		name="description"
+		placeholder="Search keyword in description..."
+		class="w-1/3"
+		bind:value={description}
+	/>
 	<div class="flex gap-2">
-		<select name="sort" placeholder="Sort by">
+		<select name="sort" placeholder="Sort by" bind:value={sortBy}>
 			<option value="date.asc">Date from earliest</option>
 			<option value="date.esc">Date from latest</option>
 			<option value="credit.asc">Amount largest to smallest</option>
 			<option value="credit.esc">Amount smallest to largest</option>
 		</select>
-		<select name="size" placeholder="Page size">
+		<select name="size" placeholder="Page size" bind:value={pageSize}>
 			<option value={10}>10</option>
 			<option value={20}>20</option>
 			<option value={50}>50</option>
 		</select>
-		<button>Filter</button>
+		<button on:click={handleFilter}>Filter</button>
 	</div>
 </div>
 <div class="h-8" />
--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,20 +1,29 @@
 export async function load() {
-  const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/v1/statements`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`
-    }
-  });
-  const data = await res.json();
+	try {
+		const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/statements`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`
+			}
+		});
+		const data = await res.json();
 
-  if (data?.rows?.length > 0) {
-    return { data };
-  }
+		if (data?.rows?.length > 0) {
+			return { data };
+		}
 
-  return {
-    data: {
-      rows: []
-    }
-  };
+		return {
+			data: {
+				rows: []
+			}
+		};
+	} catch (error) {
+		console.log(error);
+		return {
+			data: {
+				rows: []
+			}
+		};
+	}
 }
