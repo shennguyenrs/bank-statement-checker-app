2efde8596174c40e5a6e0fa08c6206883814f936
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2024-09-16 16:25:39 +0300

    fix: api should return pagination details

--- backend/src/controllers/statement.ts
+++ backend/src/controllers/statement.ts
@@ -1,4 +1,4 @@
-import { asc, desc, like } from "drizzle-orm";
+import { asc, count, desc, like } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/d1";
 import { Hono } from "hono";
 
@@ -8,7 +8,7 @@ import type { Env } from "../index";
 const router = new Hono<{ Bindings: Env }>();
 
 router.get("/", async (c) => {
-  let { name, page, size, sort } = c.req.query();
+  let { description, page, size, sort, sortBy } = c.req.query();
 
   // Validate page, size, sort
   if (!page || isNaN(Number(page))) {
@@ -19,29 +19,80 @@ router.get("/", async (c) => {
     size = "10";
   }
 
-  if (!sort || (sort !== "asc" && sort !== "desc")) {
-    sort = "asc";
-  }
-
   const pageNum = Number(page);
   const sizeNum = Number(size);
+  const orderBy = getSortBy({ sort, sortBy, sqlCols: statement });
 
   const db = drizzle(c.env.DB);
   const rows = await db
     .select()
     .from(statement)
     .where(
-      name?.trim().length > 0
-        ? like(statement.description, `%${name.trim()}%`)
+      description?.trim().length > 0
+        ? like(statement.description, `%${description.trim()}%`)
         : undefined,
     )
     .limit(sizeNum)
     .offset((pageNum - 1) * sizeNum)
-    .orderBy(sort === "asc" ? asc(statement.id) : desc(statement.id));
+    .orderBy(orderBy);
+
+  const countRows = await db
+    .select({
+      value: count()
+    })
+    .from(statement)
+    .where(
+      description?.trim().length > 0
+        ? like(statement.description, `%${description.trim()}%`)
+        : undefined,
+    )
 
   return c.json({
     rows,
+    currentPage: pageNum,
+    pageSize: sizeNum,
+    totalPages: Math.ceil(countRows[0].value / sizeNum),
   });
 });
+
+function getSortBy({
+  sort,
+  sortBy,
+  sqlCols,
+}: {
+  sort: string;
+  sortBy: string;
+  sqlCols: typeof statement;
+}) {
+  if (!sort || (sort !== "asc" && sort !== "desc")) {
+    sort = "asc";
+  }
+
+  if (sortBy !== "date" && sortBy !== "credit") {
+    sortBy = "id";
+  }
+
+  if (sortBy === "id") {
+    if (sort === "asc") {
+      return asc(sqlCols.id);
+    } else {
+      return desc(sqlCols.id);
+    }
+  } else if (sortBy === "date") {
+    if (sort === "asc") {
+      return asc(sqlCols.date);
+    } else {
+      return desc(sqlCols.date);
+    }
+  } else if (sortBy === "credit") {
+    if (sort === "asc") {
+      return asc(sqlCols.credit);
+    } else {
+      return desc(sqlCols.credit);
+    }
+  }
+
+  return asc(sqlCols.id);
+}
 
 export default router;
